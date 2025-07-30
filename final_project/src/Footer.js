e0154db7d4ff8facf0810f1e61188a057bcf61c4
Derek Lee <derek.lee@piersonwireless.com>
2025-07-30 10:33:07 -0500

    styled

--- final_project/src/Footer.js
+++ final_project/src/Footer.js
@@ -1,29 +1,31 @@
 const Footer = () => {
     return (
         <>
-            <img src="/LittleLemonLogo1.png" width="100px"/>
-            <>
+            <section className='FooterSection'>
                 <ul>
-                    <li><h3>Doormat Navigation</h3></li>
-                    <li><a href="/#home">Home</a></li>
-                    <li><a href="/#about">About</a></li>
-                    <li><a href="/#Menu">Menu</a></li>
-                    <li><a href="/#Reservations">Reservations</a></li>
-                    <li><a href="/#order-online">Order Online</a></li>
-                    <li><a href="/#login">Login</a></li>
+                    <li><img src="/LittleLemonLogo1.png" width="100px"/></li>
                 </ul>
                 <ul>
-                    <li><h3>Contact</h3></li>
-                    <li><p>Address: xyz</p></li>
-                    <li><p>Phone Number: 123</p></li>
-                    <li><p>Email: a@a.com</p></li>
+                        <h3>Doormat Navigation</h3>
+                        <a href="/#home">Home</a>
+                        <a href="/#about">About</a>
+                        <a href="/#Menu">Menu</a>
+                        <a href="/#Reservations">Reservations</a>
+                        <a href="/#order-online">Order Online</a>
+                        <a href="/#login">Login</a>
+                </ul>
+                <ul>
+                    <h3>Contact</h3>
+                    <text>Address: xyz</text>
+                    <text>Phone Number: 123</text>
+                    <text>Email: a@a.com</text>
                 </ul>
                 <ul>
                     <li><h3>Social Media Links</h3></li>
                     <li><a href="https://www.linkedin.com">LinkedIn</a></li>
                     <li><a href="https://facebook.com">Facebook</a></li>
                 </ul>
-            </>
+            </section>
         </>
     );
 }
--- final_project/src/Nav.js
+++ final_project/src/Nav.js
@@ -1,14 +1,14 @@
 const Nav = () => {
     return (
         <>
-            <img src="/LittleLemonLogo2.png" alt="Logo" width="300px"/>
-            <ul>
-                <li><a href="/#home">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#Menu">Menu</a></li>
-                <li><a href="/#Reservations">Reservations</a></li>
-                <li><a href="/#order-online">Order Online</a></li>
-                <li><a href="/#login">Login</a></li>
+            <ul className='NavGroup'>
+                <li className='NavList'><img src="/LittleLemonLogo2.png" alt="Logo" width="300px"/></li>
+                <li className='NavList'><a href="/#home">Home</a></li>
+                <li className='NavList'><a href="/#about">About</a></li>
+                <li className='NavList'><a href="/#Menu">Menu</a></li>
+                <li className='NavList'><a href="/#Reservations">Reservations</a></li>
+                <li className='NavList'><a href="/#order-online">Order Online</a></li>
+                <li className='NavList'><a href="/#login">Login</a></li>
             </ul>
         </>
     );
